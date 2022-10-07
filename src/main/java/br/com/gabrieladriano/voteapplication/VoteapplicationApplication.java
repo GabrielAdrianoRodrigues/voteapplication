a6a6957ccf5037851c12459530ffd4d90433b244
GabrielAdrianoRodrigues <gabrieladriano@furb.br>
2022-10-07 20:43:18 -0300

    initial commit

---
+++ src/main/java/br/com/gabrieladriano/voteapplication/VoteapplicationApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrieladriano.voteapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabrieladriano/voteapplication/VoteapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrieladriano.voteapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
