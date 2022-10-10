6e0c9fd516a552a9021b9d7d099497fdc0d95e0a
GabrielAdrianoRodrigues <gabrieladriano@furb.br>
2022-10-10 20:33:07 -0300

    testes

--- src/main/java/br/com/gabrieladriano/voteapplication/repositories/AssociatedRepository.java
+++ src/main/java/br/com/gabrieladriano/voteapplication/repositories/AssociatedRepository.java
@@ -15,7 +15,7 @@ public interface AssociatedRepository extends JpaRepository<Associated, Long> {
                  + "    FROM associateds                                            "
                  + "        WHERE nm_associated ILIKE :name                         "
                  + "            AND desc_cpf = :cpf                                 "   
-                 + "            AND able = Coalesce(:able, is not null)             "
+                 + "            AND (is_able = :able or :able is null )             "
                  + "                ORDER BY nm_associated                          " 
                  + "                    OFFSET :page_index                          "
                  + "                    LIMIT  :page_size                           "
